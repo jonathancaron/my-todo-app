a1c0e678fa657a564c689636a3ecb28ea7e96b5d
jonathancaron <jonathan.caron04@gmail.com>
2017-11-13 10:37:30 +0100

    Retrait colonnes et lignes (car pas la consigne)

--- index.php
+++ index.php
@@ -13,29 +13,20 @@
       <div id="main-container">
         <!--header-->
         <div class="line">
-          <div class="col un haut">
+
             <p class="title left">MY TODOLIST</p>
             <img class="right" src="./img/add.png" alt="Add Task" width="50px">
             <hr class="clear">
-          </div>
-          <div class="col deux haut">
-            <p>Add Task</p>
-          </div>
         </div>
         <div class="clear"></div>
         <!--end header-->
         <!--tasks done-->
         <div class="line">
-          <div class="col un haut">
             <ul class="p2 barre">
               <li>Take a shower </li>
               <li>Make my bag </li>
               <li>Take a breakfast </li>
             </ul>
-          </div>
-          <div class="col deux haut">
-            <p></p>
-          </div>
         </div>
         <div class="clear"></div>
         <!--end tasks done-->
