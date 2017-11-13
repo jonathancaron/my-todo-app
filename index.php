cb39a4ce27333b196bc6084971cb854203d8a1ed
jonathancaron <jonathan.caron04@gmail.com>
2017-11-13 10:49:30 +0100

    Suite

--- index.php
+++ index.php
@@ -13,23 +13,23 @@
       <div id="main-container">
         <!--header-->
         <div class="line">
-
-            <p class="title left">MY TODOLIST</p>
-            <img class="right" src="./img/add.png" alt="Add Task" width="50px">
-            <hr class="clear">
+          <p class="title left">MY TODOLIST</p>
+          <img class="right" src="./img/add.png" alt="Add Task" width="50px">
+          <hr class="clear">
         </div>
-        <div class="clear"></div>
         <!--end header-->
+        <div class="clear"></div>
         <!--tasks done-->
         <div class="line">
             <ul class="p2 barre">
-              <li>Take a shower </li>
-              <li>Make my bag </li>
-              <li>Take a breakfast </li>
+              <li><img src="./img/chip-circle-full.png" width="10px"> Take a shower </li>
+              <li><img src="./img/chip-circle-full.png" width="10px"> Make my bag </li>
+              <li><img src="./img/chip-circle-full.png" width="10px"> Take a breakfast </li>
             </ul>
         </div>
-        <div class="clear"></div>
         <!--end tasks done-->
+        <div class="clear"></div>
+
 
       </div>
 
