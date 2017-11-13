a3e5082d0a56924b4ff62d58f25a43c262d10aaf
jonathancaron <jonathan.caron04@gmail.com>
2017-11-13 10:17:30 +0100

    Add header et quelques classes css

--- index.php
+++ index.php
@@ -6,7 +6,37 @@
     <link rel="stylesheet" href="./style.css">
   </head>
   <body>
+    <div id="main">
+      <div id="main-header">
 
+      </div>
+      <div id="main-container">
+        <div class="line">
+          <div class="col un haut"><p></p>
+          </div>
+          <div class="col deux haut">
+            <p class="title left">MY TODOLIST</p>
+            <img class="right" src="./img/add.png" alt="Add Task" width="50px">
+            <hr class="clear">
+          </div>
+          <div class="col trois haut">
+            <p>Add Task</p>
+          </div>
+        </div>
+        <div class="clear"></div>
+
+
+      </div>
+
+
+      <div class="next-container">
+      </div>
+
+
+      <div id="main-footer">
+      </div>
+
+    </div>
   </body>
   <script src="./script.js"></script>
 </html>
